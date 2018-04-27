bee8b557bf57e2f5370ea5065737897629a1b563
NeoDarkFire <dark.fire@gmx.fr>
2018-04-27 10:59:06 +0200

    DAOHelloWorld : file reader Singleton

---
+++ helloworldmaven.model/src/main/java/helloworldmaven/model/Model.java
@@ -0,0 +1,11 @@
+package helloworldmaven.model;
+
+import helloworldmvc.contract.IModel;
+
+public class Model implements IModel {
+
+	public String getHelloWorld() {
+		return DAOHelloWorld.getInstance().getHelloWorldMessage();
+	}
+
+}
